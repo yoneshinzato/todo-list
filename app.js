66ee80f251850f05e110e39860bf0728b40dee11
yoneshinzato <chibariyo@gmail.com>
2021-02-03 22:53:37 -0300

    adding todos

---
+++ app.js
@@ -0,0 +1,25 @@
+const addForm = document.querySelector('.add')
+const list = document.querySelector('.todos')
+
+//function to generate template to add todo
+const generateTemplate = (todo) => {
+    const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${todo}</span>
+    <i class="far fa-trash-alt delete"></i>
+  </li>
+    `;
+    list.innerHTML += html
+}
+
+addForm.addEventListener('submit', e => {
+
+    e.preventDefault()
+    const todo = addForm.add.value.trim() //add input's name
+
+    //check if something was typed
+    if(todo.length){
+        generateTemplate(todo)
+        addForm.reset()
+    } 
+})
